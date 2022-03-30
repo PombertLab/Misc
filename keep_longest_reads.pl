cbd7dac92fd090dadf27ec4f671449ff99fb5235
Jean-Francois Pombert <jpombert@iit.edu>
2022-03-30 17:56:59 -0500

    works but needs polishing

--- keep_longest_reads.pl
+++ keep_longest_reads.pl
@@ -8,6 +8,8 @@ use strict;
 use warnings;
 use Getopt::Long qw(GetOptions);
 use PerlIO::gzip;
+use File::Basename;
+use File::Path qw(make_path);
 
 ## Usage definition
 my $usage = <<"USAGE";
@@ -22,204 +24,305 @@ EXAMPLE 2	${name} -i file.fastq -o parsed.100x.fastq -d 100 -s 3000000
 EXAMPLE 3	${name} -i file.fastq -x
 
 OPTIONS
--i (--input)	Input file in FASTQ format
+-i (--input)	Input file(s) in FASTQ format
 -x (--metrics)	Calculate metrics only, do not create read subset
--o (--output)	Output file name
+-o (--outdir)	Output directory [Default: ./]
 -m (--minimum)	Minimum read length to keep
 -d (--depth)	Desired sequencing depth (requires estimated genome size: -s)
 -s (--size)	Expected genome size
+-j (--json)	Create .json file for multiQC
 
 NOTE: -m and -d are mutually exclusive
 USAGE
 die "\n$usage\n" unless@ARGV;
 
 my @commands = @ARGV;
-my $fastq;
+my @fastq;
 my $metrics;
-my $output;
+my $outdir = './';
 my $min;
 my $depth;
 my $genome_size;
+my $json;
 GetOptions(
-	'i|input=s' => \$fastq,
+	'i|input=s@{1,}' => \@fastq,
 	'x|metrics'	=> \$metrics,
-	'o|output=s' => \$output,
+	'o|outdir=s' => \$outdir,
 	'm|minimum=i' => \$min,
 	'd|depth=i' => \$depth,
-	's|size=i' => \$genome_size
+	's|size=i' => \$genome_size,
+	'j|json' => \$json
 );
 
-## Input file
-my $gzip = '';
-if ($fastq =~ /.gz$/){ $gzip = ':gzip'; }
-open FASTQ1, "<$gzip", "$fastq" or die "Can't open $fastq: $!\n";
-open FASTQ2, "<$gzip", "$fastq" or die "Can't open $fastq: $!\n";
-
-## Log file
-my $logfile;
-if ($metrics){ $logfile = 'metrics.log'; }
-else  { $logfile = "$output.log"; }
-open LOG, ">", "$logfile" or die "Can't create $logfile: $!\n";
-
-## Output file
-unless ($metrics){
-	open OUT, ">", "$output" or die "Can't create $output: $!\n";
+## Outdir + logfile
+unless (-d $outdir) {
+	make_path( $outdir, { mode => 0755 } )  or die "Can't create $outdir: $!\n";
 }
+
+my $logfile = 'klr_metrics.log';
+open LOG, ">", "$outdir/$logfile" or die "Can't create $outdir/$logfile: $!\n";
 
 ## Log file
 my $stime = `date`; chomp $stime;
 print LOG "COMMAND: $name @commands\n";
 print LOG "Started on $stime\n";
 
-## parsing by minimum length
-if ($min or $metrics){
+## working on input file(s)
+my %json_data;
+my $basename;
 
-    my @lengths; my @subset; my %reads; my $count = 0; my $read;
+while (my $fastq = shift@fastq){
 
-	if ($metrics){ $min = 1; }
+	my $gzip = '';
+	if ($fastq =~ /.gz$/){ $gzip = ':gzip'; }
+	open FASTQ1, "<$gzip", "$fastq" or die "Can't open $fastq: $!\n";
+	open FASTQ2, "<$gzip", "$fastq" or die "Can't open $fastq: $!\n";
 
-	while (my $line = <FASTQ1>){
-		chomp $line;
+	$basename = fileparse($fastq);
+	$basename =~ s/\.gz$//;
+	$basename =~ s/\.fastq$//;
+	$basename =~ s/\.fq$//;
 
-		## Read name
-		if ($count == 0){
-			$read = $line;
-			$reads{$read}[0] = $line;
-			$count++;
+	## parsing by minimum length
+	if ($min or $metrics){
+
+		## Output file
+		my $filename;
+		if ($min){
+			my $div = ($min/1000);
+			$filename = $outdir."/".$basename.'_'.$div."k.fastq";
+		}
+		unless ($metrics){
+			open OUT, ">", "$filename" or die "Can't create $filename: $!\n";
 		}
 
-		## Read sequence
-		elsif ($count == 1){ 
-			$reads{$read}[1] = $line;
-			## Adding leading zeroes to help sort array
-			my $len = sprintf("%09d", length($line));
-			push (@lengths, $len);
-			$count++;
+		my $minimum_len;
+		if ($min){ $minimum_len = $min; }
+		else {
+			$minimum_len = 1;
 		}
 
-		## Skipping + sign
-		elsif ($count == 2){ $count++; }
+		my @lengths; my @subset; my %reads; my $count = 0; my $read;
+		while (my $line = <FASTQ1>){
+			chomp $line;
 
-		## Quality score 
-		elsif ($count == 3){
-			$reads{$read}[2] = $line;
-			if (length($reads{$read}[1]) >= $min){
-				my $keep = sprintf("%09d", length($reads{$read}[1]));
-				push (@subset, $keep);
-				unless ($metrics){
+			## Read name
+			if ($count == 0){
+				$read = $line;
+				$reads{$read}[0] = $line;
+				$count++;
+			}
+
+			## Read sequence
+			elsif ($count == 1){ 
+				$reads{$read}[1] = $line;
+				## Adding leading zeroes to help sort array
+				my $len = sprintf("%09d", length($line));
+				push (@lengths, $len);
+				$count++;
+			}
+
+			## Skipping + sign
+			elsif ($count == 2){ $count++; }
+
+			## Quality score 
+			elsif ($count == 3){
+				$reads{$read}[2] = $line;
+				if (length($reads{$read}[1]) >= $minimum_len){
+					my $keep = sprintf("%09d", length($reads{$read}[1]));
+					push (@subset, $keep);
+					unless ($metrics){
+						print OUT "$reads{$read}[0]\n";
+						print OUT "$reads{$read}[1]\n";
+						print OUT '+'."\n";
+						print OUT "$reads{$read}[2]\n";
+					}
+				}
+				## Clearing read db to minimize memory usage
+				$count = 0; %reads = (); 
+			}
+		}
+
+		if ($fastq =~ /.gz$/){ binmode FASTQ1, ":gzip(none)"; }	
+
+		n50($fastq, 'full', @lengths); ## Full
+		if ($min){
+			n50($filename, $min, @subset); ## subset
+		}
+		unless ($metrics){
+			close OUT;
+		}
+
+	}
+
+	elsif ($depth){
+
+		## Output file
+		my $filename = $outdir."/".$basename.'_'.$min."x.fastq";
+		unless ($metrics){
+			open OUT, ">", "$filename" or die "Can't create $filename: $!\n";
+		}
+
+		my @lengths;
+		my $count = 0;
+		## Doing two independent passes to avoid loading the full FASTQ file in memory
+		## Pass 1 - Calculating metrics
+		while (my $line = <FASTQ1>){
+			chomp $line;
+			if ($count == 0){ $count++; }
+			elsif ($count == 1){
+				## Adding leading zeroes to help sort array
+				my $len = sprintf("%09d", length($line));
+				push (@lengths, $len);
+				$count++;
+			}
+			elsif ($count == 2){ $count++; }
+			elsif ($count == 3){ $count=0; }
+		}
+
+		if ($fastq =~ /.gz$/){ binmode FASTQ1, ":gzip(none)"; }	
+
+		@lengths = sort @lengths; ## sort by size
+		@lengths = reverse @lengths; ## from largest to smallest
+		my $len_threshold; my $sum;
+		foreach (@lengths){
+			if ($sum <= ($depth*$genome_size)){ $len_threshold = $_; }
+			$sum += $_;
+		}
+		$len_threshold = sprintf("%0d", $len_threshold);
+		print "\nMinimum read size for ${depth}X sequencing depth at estimated genome size of $genome_size bp = $len_threshold bp\n";
+		print "Saving reads of at least $len_threshold bp to $filename\n\n";
+
+		## Pass 2 - Parsing reads
+		$count = 0; my $read; my %reads; my @subset;
+		while (my $line = <FASTQ2>){
+			chomp $line;
+
+			## Read name
+			if ($count == 0){
+				$read = $line;
+				$reads{$read}[0] = $line;
+				$count++;
+			}
+
+			## Read sequence
+			elsif ($count == 1){ 
+				$reads{$read}[1] = $line;
+				$count++;
+			}
+
+			## Skipping + sign
+			elsif ($count == 2){ $count++; }
+
+			## Quality score
+			elsif ($count == 3){
+				$reads{$read}[2] = $line; 
+				if (length($reads{$read}[1]) >= $len_threshold){
+					my $keep = sprintf("%09d", length($reads{$read}[1]));
+					push (@subset, $keep);
 					print OUT "$reads{$read}[0]\n";
 					print OUT "$reads{$read}[1]\n";
 					print OUT '+'."\n";
 					print OUT "$reads{$read}[2]\n";
 				}
+				## Clearing read db to minimize memory usage
+				$count = 0; %reads = ();
 			}
-			## Clearing read db to minimize memory usage
-			$count = 0; %reads = (); 
 		}
+
+		if ($fastq =~ /.gz$/){ binmode FASTQ2, ":gzip(none)"; }
+
+		n50($fastq, 'full', @lengths); ## Full
+		n50($filename, $depth, @subset); ## Subset
+		close OUT;
 	}
 
-	if ($fastq =~ /.gz$/){ binmode FASTQ1, ":gzip(none)"; }	
+	close FASTQ1;
+	close FASTQ2;
 
-	n50($fastq, @lengths);
-	unless ($metrics){
-		n50($output, @subset);
-	}
 }
 
-elsif ($depth){
-	my @lengths;
-	my $count = 0;
-	## Doing two independent passes to avoid loading the full FASTQ file in memory
-	## Pass 1 - Calculating metrics
-	while (my $line = <FASTQ1>){
-		chomp $line;
-		if ($count == 0){ $count++; }
-		elsif ($count == 1){
-			## Adding leading zeroes to help sort array
-			my $len = sprintf("%09d", length($line));
-			push (@lengths, $len);
-			$count++;
+############## .json file for MultiQC
+if ($json){
+	my $json_file = $outdir.'/klr_mqc.json';
+	open MQC, ">", "$json_file" or die "Can't create $json_file: $!\n";
+
+	my $header = <<"	HEAD";
+	"id": "KLR",
+	"section_name": "Long read data",
+	"description": "Metrics calculated with keep_longest_reads.pl (https://github.com/PombertLab).",
+	"plot_type": "table",
+	"pconfig": {
+		"id": "KLR",
+		"format": "{:,.0f}"
+	},
+	HEAD
+
+	print MQC "\{\n";
+	print MQC "$header";
+	print MQC "\t\"data\": {\n";
+	my $comma_counter = 0;
+	foreach my $key (sort (keys %json_data) ){
+		print MQC "\t\t\"$key\": {\n";
+		print MQC "\t\t\t\"Number of reads\": $json_data{$key}{'nreads'},\n";
+		print MQC "\t\t\t\"Total number of bases\": $json_data{$key}{'total'},\n";
+		print MQC "\t\t\t\"Longest read\": $json_data{$key}{'longest'},\n";
+		print MQC "\t\t\t\"Shortest read\": $json_data{$key}{'shortest'},\n";
+		print MQC "\t\t\t\"Average read size\": $json_data{$key}{'average'},\n";
+		print MQC "\t\t\t\"Median read size\": $json_data{$key}{'median'},\n";
+		print MQC "\t\t\t\"N50\": $json_data{$key}{'n50'},\n";
+		print MQC "\t\t\t\"N75\": $json_data{$key}{'n75'},\n";
+		print MQC "\t\t\t\"N90\": $json_data{$key}{'n90'}\n";
+		unless ($comma_counter == scalar (keys %json_data) - 1){
+			print MQC "\t\t},\n";
 		}
-		elsif ($count == 2){ $count++; }
-		elsif ($count == 3){ $count=0; }
+		else {
+			print MQC "\t\t}\n";
+		}
+		$comma_counter++;
 	}
 
-	if ($fastq =~ /.gz$/){ binmode FASTQ1, ":gzip(none)"; }	
+	print MQC "\t}\n}";
 
-	@lengths = sort @lengths; ## sort by size
-	@lengths = reverse @lengths; ## from largest to smallest
-	my $len_threshold; my $sum;
-	foreach (@lengths){
-		if ($sum <= ($depth*$genome_size)){ $len_threshold = $_; }
-		$sum += $_;
-	}
-	$len_threshold = sprintf("%0d", $len_threshold);
-	print "\nMinimum read size for ${depth}X sequencing depth at estimated genome size of $genome_size bp = $len_threshold bp\n";
-	print "Saving reads of at least $len_threshold bp to $output\n\n";
-
-	## Pass 2 - Parsing reads
-	$count = 0; my $read; my %reads; my @subset;
-	while (my $line = <FASTQ2>){
-		chomp $line;
-
-		## Read name
-		if ($count == 0){
-			$read = $line;
-			$reads{$read}[0] = $line;
-			$count++;
-		}
-
-		## Read sequence
-		elsif ($count == 1){ 
-			$reads{$read}[1] = $line;
-			$count++;
-		}
-
-		## Skipping + sign
-		elsif ($count == 2){ $count++; }
-
-		## Quality score
-		elsif ($count == 3){
-			$reads{$read}[2] = $line; 
-			if (length($reads{$read}[1]) >= $len_threshold){
-				my $keep = sprintf("%09d", length($reads{$read}[1]));
-				push (@subset, $keep);
-				print OUT "$reads{$read}[0]\n";
-				print OUT "$reads{$read}[1]\n";
-				print OUT '+'."\n";
-				print OUT "$reads{$read}[2]\n";
-			}
-			## Clearing read db to minimize memory usage
-			$count = 0; %reads = ();
-		}
-	}
-
-	if ($fastq =~ /.gz$/){ binmode FASTQ2, ":gzip(none)"; }	
-
-	n50($fastq, @lengths);
-	n50($output, @subset);
 }
 
+############## Final timestamp
 my $etime = `date`; chomp $etime;
 unless ($metrics){
 	print LOG "Ended on: $etime\n";
 }
 
-### subroutines
+################################################################################
+## subroutines
+
 sub n50{
+
 	my @fh = (*LOG, *STDOUT);
 	my $file = shift @_;
+	my $basename_ext = shift @_;
 	my $num_reads = scalar @_;
 	my @len = sort @_; ## sort by size
 	@len = reverse @len; ## from largest to smallest
 
+	unless ($basename_ext eq 'full'){
+		if ($min){
+			my $div = ($basename_ext/1000);
+			$basename = $basename." >= ".$div.'k';
+		}
+		elsif ($depth){
+			$basename = $basename." ".$basename_ext.'x';
+		}
+		
+	}
+
+	$json_data{$basename}{'nreads'} = $num_reads;
 	my $nreads = commify($num_reads);
 	foreach (@fh){
 		print $_ "\n## Metrics for dataset $file\n\n";
 		print $_ "Number of reads: $nreads\n";
 	}
 
-	## Median
+	# median
 	my $median;
 	my $median_pos = $num_reads/2;
 	if ($median_pos =~ /^\d+$/){ $median = $len[$median_pos]; }
@@ -228,14 +331,30 @@ sub n50{
 		my $med2 = $med1 + 1;
 		$median = (($len[$med1] + $len[$med2])/2);
 	}
+	$median = sprintf("%.0f", $median);
+	$json_data{$basename}{'median'} = $median;
+	$median = commify($median);
 
-	## Average
+	# sum
 	my $sum; foreach (@len){ $sum += $_; }
+	$json_data{$basename}{'total'} = $sum;
 	my $fsum = commify($sum);
-	my $large = sprintf("%.0f", $len[0]); $large = commify($large);
-	my $small = sprintf("%.0f", $len[$#len]); $small = commify($small);
-	my $average = sprintf("%.0f", ($sum/$num_reads)); $average = commify($average);
-	$median = sprintf("%.0f", $median); $median = commify($median);
+
+	# longest
+	my $large = sprintf("%.0f", $len[0]);
+	$json_data{$basename}{'longest'} = $large;
+	$large = commify($large);
+
+	# shortest
+	my $small = sprintf("%.0f", $len[$#len]);
+	$json_data{$basename}{'shortest'} = $small;
+	$small = commify($small);
+
+	# average
+	my $average = sprintf("%.0f", ($sum/$num_reads));
+	$json_data{$basename}{'average'} = $average;
+	$average = commify($average);
+
 	foreach (@fh){
 		print $_ "Total number of bases: $fsum\n";
 		print $_ "Longest read = $large nt\n";
@@ -245,16 +364,36 @@ sub n50{
 	}
 
 	## N50, N75, N90
-	my $n50_td = $sum*0.5; my $n75_td = $sum*0.75; my $n90_td = $sum*0.9;
+	my $n50_td = $sum*0.5;
+	my $n75_td = $sum*0.75;
+	my $n90_td = $sum*0.9;
 	my $n50; my $n75, my $n90;
 	my $nsum50 = 0; my $nsum75 = 0; my $nsum90 = 0;
+
 	foreach (@len){ $nsum50 += $_; if ($nsum50 >= $n50_td){ $n50 = $_; last; }}
 	foreach (@len){ $nsum75 += $_; if ($nsum75 >= $n75_td){ $n75 = $_; last; }}
 	foreach (@len){ $nsum90 += $_; if ($nsum90 >= $n75_td){ $n90 = $_; last; }}
-	$n50 = sprintf ("%.0f", $n50); $n50 = commify($n50);
-	$n75 = sprintf ("%.0f", $n75); $n75 = commify($n75);
-	$n90 = sprintf ("%.0f", $n90); $n90 = commify($n90);
-	foreach (@fh){ print $_ "N50 = $n50 nt\n"."N75 = $n75 nt\n"."N90 = $n90 nt\n"."\n"; }
+
+	# n50
+	$n50 = sprintf ("%.0f", $n50);
+	$json_data{$basename}{'n50'} = $n50;
+	$n50 = commify($n50);
+
+	$n75 = sprintf ("%.0f", $n75);
+	$json_data{$basename}{'n75'} = $n75;
+	$n75 = commify($n75);
+
+	$n90 = sprintf ("%.0f", $n90);
+	$json_data{$basename}{'n90'} = $n90;
+	$n90 = commify($n90);
+
+	foreach (@fh){
+		print $_ "N50 = $n50 nt\n";
+		print $_ "N75 = $n75 nt\n";
+		print $_ "N90 = $n90 nt\n";
+		print $_ "\n";
+	}
+
 }
 
 sub commify { ## From the Perl Cookbook; O'Reilly
