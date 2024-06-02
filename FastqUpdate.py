8e52d26e2d40ba12a4003997ea509634372d2c19
Jean-Francois Pombert <jpombert@iit.edu>
2024-06-02 08:53:41 -0400

    minor code cleanup

--- FastqUpdate.py
+++ FastqUpdate.py
@@ -1,86 +1,93 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+
+name = "FastqUpdate.py"
+version = '0.2a'
+updated = '2024-06-02'
+
 from Bio import SeqIO
 import argparse
 from os import path 
 from sys import exit,argv
 
-name = "FastqUpdate.py"
-version = '0.2'
-updated = '2021-03-21'
+#########################################################################
+### Command line options
+#########################################################################
 
 usage = f"""
-NAME		{name}
-VERSION		{version}
-UPDATED		{updated}
-SYNOPSIS	The purpse of this script is to convert sequence files of one encoding
-			into sequence files of another encoding type
+NAME        {name}
+VERSION     {version}
+UPDATED     {updated}
+SYNOPSIS    Convert sequence file formats using biopython
 
-COMMAND		{name} \\
-			--input raw_R1.fastq \\
-			--encode fastq-illumina
+REQS        python3, biopython
 
--i | --input	Input sequence file
--e | --encode	Input sequence encoding
--n | --nencode	Output sequence encoding [default = fastq]
--o | --output	Output file name [default = <InputFileName>_converted.<nencode>]
+COMMAND     {name} \\
+              --input raw_R1.fastq \\
+              --encode fastq-illumina
 
-Supported Sequence Encoding Types
----------------------------------
-KEY: r|ead w|rite
-
-r-|abi
-r-|abit-trim
-r-|ace
-r-|cif-atom
-r-|cif-seqres
-rw|clustal
-rw|embl
-rw|fasta
-rw|fasta-2line
-rw|fastq-sanger/fastq
-rw|fastq-solexa
-rw|fastq-illumina
-r-|gck
-rw|genebank/gb
-r-|ig
-rw|imgt
-rw|nexus
-r-|pbd-seqres
-r-|pdv-aom
-rw|phd
-rw|phylip
-rw|pir
-rw|seqxml
-rw|sff
-r-|sff-trim
-r-|snapgene
-rw|stockholm
-r-|swiss
-rw|tab
-rw|qual 
-r-|uniprot-xml 
-rw|xda
-
+-i (--input)    Input sequence file
+-e (--encode)   Input sequence encoding
+-n (--nencode)  Output sequence encoding [default = fastq]
+-o (--output)   Output file name [default = <InputFileName>_converted.<nencode>]
+-f (--formats)  Show supported formats
+-v (--version)  Show script version
 """
 
-# Die usage unless argv is populated
-if len(argv) == 1:
+formats = f"""
+Supported Sequence Encoding Types
+---------------------------------
+r = read, w = write
+
+r-  abi
+r-  abit-trim
+r-  ace
+r-  cif-atom
+r-  cif-seqres
+rw  clustal
+rw  embl
+rw  fasta
+rw  fasta-2line
+rw  fastq-sanger/fastq
+rw  fastq-solexa
+rw  fastq-illumina
+r-  gck
+rw  genebank/gb
+r-  ig
+rw  imgt
+rw  nexus
+r-  pbd-seqres
+r-  pdv-aom
+rw  phd
+rw  phylip
+rw  pir
+rw  seqxml
+rw  sff
+r-  sff-trim
+r-  snapgene
+rw  stockholm
+r-  swiss
+rw  tab
+rw  qual 
+r-  uniprot-xml 
+rw  xda
+"""
+
+# Print custom message if argv is empty
+if len(argv) <= 1:
     print(usage)
-    exit()
+    exit(0)
 
 ## Setting up default variable
 nencode = "fastq"
 
-## Set up command line parser
+## Set up command line parser + command line flags
 parser = argparse.ArgumentParser(usage=usage)
-
-## Create command line flag
-args = parser.add_argument("-i","--input",required=True)
-args = parser.add_argument("-e","--encode",required=True)
+args = parser.add_argument("-i","--input")
+args = parser.add_argument("-e","--encode")
 args = parser.add_argument("-o","--output")
 args = parser.add_argument("-n","--nencode")
-
-## Create parser object
+args = parser.add_argument("-f","--formats", action='store_true')
+args = parser.add_argument("-v","--version", action='store_true')
 args = parser.parse_args()
 
 ## Parse command line objects by <parser_object>.<flag_word>
@@ -88,6 +95,31 @@ in_file = args.input
 encode = args.encode
 out_file = args.output
 nencode = args.nencode
+sformats = args.formats
+scversion = args.version
+
+#########################################################################
+### Version
+#########################################################################
+
+if scversion:
+    print ("")
+    print (f"Script:     {name}")
+    print (f"Version:    {version}")
+    print (f"Updated:    {updated}\n")
+    exit(0)
+
+#########################################################################
+### Supported formats
+#########################################################################
+
+if sformats:
+    print (formats)
+    exit(0)
+
+#########################################################################
+### File conversion
+#########################################################################
 
 ## Parsing name and extension of input file provided
 base_name, file_name = path.split(in_file)
@@ -95,7 +127,7 @@ file_name, file_ext = path.splitext(file_name)
 
 ## If output file not given, create default from input file
 if not out_file:
-	out_file = file_name + "_converted" + nencode
+    out_file = file_name + "_converted" + nencode
 
 ## If directory of file is current working folder, make note
 if not base_name:
--- read_len_plot.py
+++ read_len_plot.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 ## Pombert lab, 2022
-version = '0.5g'
-updated = '2024-04-16'
+
+version = '0.6'
+updated = '2024-06-02'
 name = 'read_len_plot.py'
 
 import os
@@ -17,48 +18,48 @@ from tqdm import tqdm
 ################################################################################
 
 usage = f"""
-NAME		{name}
-VERSION		{version}
-UPDATED		{updated}
-SYNOPSIS	Plots the read length distribution for a given FASTQ dataset with 
-		matplotlib
+NAME        {name}
+VERSION     {version}
+UPDATED     {updated}
+SYNOPSIS    Plots the read length distribution for a given FASTQ dataset with 
+            matplotlib
 
-REQS		pragzip - https://pypi.org/project/pragzip/ 
-		tqdm - https://pypi.org/project/tqdm/
-		## pip install pragzip tqdm
+REQS        pragzip - https://pypi.org/project/pragzip/ 
+            tqdm - https://pypi.org/project/tqdm/
+            ## pip install pragzip tqdm
 
-COMMAND		{name} \\
-		  -f reads.fastq \\
-		  -c darkorange \\
-		  -o read_distribution.svg read_distribution.pdf \\
-		  -x 50000
+COMMAND     {name} \\
+              -f reads.fastq \\
+              -c darkorange \\
+              -o read_distribution.svg read_distribution.pdf \\
+              -x 50000
 
 I/O OPTIONS:
--f (--fastq)	FASTQ file to plot (GZIP files are supported)
--d (--outdir)	Output directory [Default: ./]
--m (--metrics)	Metrics output file [Default: read_metrics.txt]
--v (--verbose)	Print progress (every 25,000 reads)
--o (--output)	Save plot to specified output file(s)
-		## Defaults to matplotlib GUI otherwize
-		## Supported formats: png, pdf, ps and svg
+-f (--fastq)      FASTQ file to plot (GZIP files are supported)
+-d (--outdir)     Output directory [Default: ./]
+-m (--metrics)    Metrics output file [Default: read_metrics.txt]
+-v (--verbose)    Print progress (every 25,000 reads)
+-o (--output)     Save plot to specified output file(s); png, pdf, ps and/or svg
+                  ## Defaults to matplotlib GUI otherwize
+--version         Show script version
 
 PLOT OPTIONS:
--c (--color)	Color to use; red, green, blue... [Default: green]
-		# https://matplotlib.org/stable/gallery/color/named_colors.html
--b (--bar)	Bar type: Read sum or read count [Default: sum]
--h (--height)	Figure height in inches [Default: 10.8]
--w (--width)	Figure width in inches [Default: 19.2]
--x (--xmax)	Set max X-axis value [Default: automatic]
--t (--ticks)	Set ticks every X kb [Default: 5]
--y (--yscale)	Set yscale: linear or log [Default: linear]
---title		Set title; defaults to file basename if not set
---title_font	Set title font: normal, bold, heavy [Default: normal]
+-c (--color)      Color to use; red, green, blue... [Default: green]
+                  # https://matplotlib.org/stable/gallery/color/named_colors.html
+-b (--bar)        Bar type: Read sum or read count [Default: sum]
+-h (--height)     Figure height in inches [Default: 10.8]
+-w (--width)      Figure width in inches [Default: 19.2]
+-x (--xmax)       Set max X-axis value [Default: automatic]
+-t (--ticks)      Set ticks every X kb [Default: 5]
+-y (--yscale)     Set yscale: linear or log [Default: linear]
+--title           Set title; defaults to file basename if not set
+--title_font      Set title font: normal, bold, heavy [Default: normal]
 """
 
 # Print custom message if argv is empty
 if (len(sys.argv) <= 1):
-	print(usage)
-	sys.exit()
+    print(usage)
+    exit(0)
 
 ################################################################################
 ## Create command lines switches
@@ -79,6 +80,7 @@ cmd.add_argument("-t", "--ticks", type=int, default=5)
 cmd.add_argument("-y", "--yscale", default='linear', choices=['linear', 'log'])
 cmd.add_argument("--title")
 cmd.add_argument("--title_font", default='normal', choices=['normal', 'bold', 'heavy'])
+args = cmd.add_argument("--version", action='store_true')
 args = cmd.parse_args()
 
 fastq = args.fastq
@@ -95,17 +97,29 @@ set_ticks = args.ticks
 yscale = args.yscale
 title = args.title
 title_font = args.title_font
+scversion = args.version
+
+################################################################################
+## Version
+################################################################################
+
+if scversion:
+    print ("")
+    print (f"Script:     {name}")
+    print (f"Version:    {version}")
+    print (f"Updated:    {updated}\n")
+    exit(0)
 
 ################################################################################
 ## Working on output directory
 ################################################################################
 
 if output is not None:
-	if os.path.isdir(outdir) == False:
-		try:
-			os.makedirs(outdir)
-		except:
-			sys.exit(f"Can't create directory {outdir}...")
+    if os.path.isdir(outdir) == False:
+        try:
+            os.makedirs(outdir)
+        except:
+            sys.exit(f"Can't create directory {outdir}...")
 
 
 ################################################################################
@@ -126,9 +140,9 @@ zipflag = check_gzip(fastq)
 FH = None
 
 if (zipflag == True):
-	FH = gzip.open(fastq,'r')
+    FH = gzip.open(fastq,'r')
 else:
-	FH = open(fastq,'r')
+    FH = open(fastq,'r')
 
 print(f"\nWorking on {fastq}...\n")
 
@@ -136,48 +150,48 @@ num_reads = None
 
 ## Count lines/reads
 if zipflag == True:
-	with pragzip.open(fastq) as file:
-		while chunk := file.read( 1024*1024 ):
-			num_lines += chunk.count(b'\n')
+    with pragzip.open(fastq) as file:
+        while chunk := file.read( 1024*1024 ):
+            num_lines += chunk.count(b'\n')
 
-	num_reads = int(num_lines / 4)
-	print(f"Total number of reads: {num_reads:,}")
+    num_reads = int(num_lines / 4)
+    print(f"Total number of reads: {num_reads:,}")
 
 if zipflag == False:
 
-	def _line_counter(reader):
-		b = reader(1024 * 1024)
-		while b:
-			yield b
-			b = reader(1024 * 1024)
+    def _line_counter(reader):
+        b = reader(1024 * 1024)
+        while b:
+            yield b
+            b = reader(1024 * 1024)
 
-	with open(fastq, 'rb') as f:
-		line_count = _line_counter(f.raw.read)
-		num_lines = sum(buffer.count(b'\n') for buffer in line_count)
-		num_reads = int(num_lines / 4)
-		print(f"Total number of reads: {num_reads:,}")
+    with open(fastq, 'rb') as f:
+        line_count = _line_counter(f.raw.read)
+        num_lines = sum(buffer.count(b'\n') for buffer in line_count)
+        num_reads = int(num_lines / 4)
+        print(f"Total number of reads: {num_reads:,}")
 
 if verbose:
-	pbar = tqdm(desc='Progress', total = num_reads)
+    pbar = tqdm(desc='Progress', total = num_reads)
 
 ## Parse reads
 for line in FH:
 
-	line_counter += 1
+    line_counter += 1
 
-	if (line_counter == 2):
-		line = line.strip()
-		read_size = len(line)
-		read_sizes.append(read_size)
+    if (line_counter == 2):
+        line = line.strip()
+        read_size = len(line)
+        read_sizes.append(read_size)
 
-	elif (line_counter == 4):
-		line_counter = 0
-		read_num += 1
-		if verbose:
-			pbar.update()
+    elif (line_counter == 4):
+        line_counter = 0
+        read_num += 1
+        if verbose:
+            pbar.update()
 
 if verbose:
-	pbar.close()
+    pbar.close()
 
 ################################################################################
 ## Calculate read metrics
@@ -198,18 +212,18 @@ median = "{:,}".format(median)
 
 # Function to calculate n metrics; e.g n50, n75, n90
 def n_metric(list, n):
-	n_threshold = int(sum(list)*n)
-	n_sum = 0
-	nmetric = 0
+    n_threshold = int(sum(list)*n)
+    n_sum = 0
+    nmetric = 0
 
-	for x in list:
-		n_sum += x
-		if n_sum >= n_threshold:
-			nmetric = x
-			break
+    for x in list:
+        n_sum += x
+        if n_sum >= n_threshold:
+            nmetric = x
+            break
 
-	nmetric = "{:,}".format(nmetric)
-	return nmetric
+    nmetric = "{:,}".format(nmetric)
+    return nmetric
 
 # n50, 75, 90
 read_sizes.sort(reverse=True)
@@ -232,11 +246,11 @@ N90:\t\t{n90}
 """
 
 if output is None:
-	print(pmetrics)
+    print(pmetrics)
 else:
-	metrics_output = outdir + '/' + metrics_file
-	METRICS = open(metrics_output,'w')
-	print(pmetrics, file=METRICS)
+    metrics_output = outdir + '/' + metrics_file
+    METRICS = open(metrics_output,'w')
+    print(pmetrics, file=METRICS)
 
 
 ################################################################################
@@ -248,19 +262,19 @@ else:
 adjust_l = 0
 metrics_list = [read_sum, read_num, longest_read, shortest_read, average, median, n50, n75, n90]
 for metric in metrics_list:
-	if len(metric) > adjust_l:
-		adjust_l = len(metric)
+    if len(metric) > adjust_l:
+        adjust_l = len(metric)
 
 metrics = f"""
-	Total bases: {read_sum.rjust(adjust_l + 1)}
-	# reads: {read_num.rjust(adjust_l + 1)}
-	Longest: {longest_read.rjust(adjust_l + 1)}
-	Shortest: {shortest_read.rjust(adjust_l + 1)}
-	Average: {average.rjust(adjust_l + 1)}
-	Median: {median.rjust(adjust_l + 1)}
-	N50: {n50.rjust(adjust_l + 1)}
-	N75: {n75.rjust(adjust_l + 1)}
-	N90: {n90.rjust(adjust_l + 1)}
+    Total bases: {read_sum.rjust(adjust_l + 1)}
+    # reads: {read_num.rjust(adjust_l + 1)}
+    Longest: {longest_read.rjust(adjust_l + 1)}
+    Shortest: {shortest_read.rjust(adjust_l + 1)}
+    Average: {average.rjust(adjust_l + 1)}
+    Median: {median.rjust(adjust_l + 1)}
+    N50: {n50.rjust(adjust_l + 1)}
+    N75: {n75.rjust(adjust_l + 1)}
+    N90: {n90.rjust(adjust_l + 1)}
 """
 metrics = metrics.replace("\t","")
 
@@ -269,16 +283,16 @@ metrics = metrics.replace("\t","")
 # Bin size + dictionary to store read size distributions
 binsize = 1000
 reads_distr = {
-	'sum': {},
-	'count': {}
+    'sum': {},
+    'count': {}
 }
 
 # Checking for max value, either from data or the command line
 max_val = None
 if xmax is None:
-	max_val = max(read_sizes)
+    max_val = max(read_sizes)
 else:
-	max_val = xmax
+    max_val = xmax
 
 # Creating bins
 num_bins = int(max_val/binsize) + 1
@@ -286,36 +300,36 @@ key_labels = []
 
 # Creating bin labels
 for x in range(0,num_bins):
-	label = f"{x}k"
-	key_labels.append(label)
-	reads_distr['sum'][label] = 0
-	reads_distr['count'][label] = 0
+    label = f"{x}k"
+    key_labels.append(label)
+    reads_distr['sum'][label] = 0
+    reads_distr['count'][label] = 0
 
 # Calculating sum of all bases (in Mb) + read count per bin
 for size in read_sizes:
-	bin_loc = int(size/binsize)
-	bin_loc = f"{bin_loc}k"
-	read_mb = size/1000000
-	if bin_loc in reads_distr[bar].keys():
-		reads_distr['sum'][bin_loc] += read_mb
-		reads_distr['count'][bin_loc] += 1
-	else:
-		reads_distr['sum'][bin_loc] = read_mb
-		reads_distr['count'][bin_loc] = 1
+    bin_loc = int(size/binsize)
+    bin_loc = f"{bin_loc}k"
+    read_mb = size/1000000
+    if bin_loc in reads_distr[bar].keys():
+        reads_distr['sum'][bin_loc] += read_mb
+        reads_distr['count'][bin_loc] += 1
+    else:
+        reads_distr['sum'][bin_loc] = read_mb
+        reads_distr['count'][bin_loc] = 1
 
 # Setting ticks for plot
 ticks = []
 labels = []
 for i in range(0, num_bins, set_ticks):
-	ticks.append(i)
-	labels.append(key_labels[i])
+    ticks.append(i)
+    labels.append(key_labels[i])
 
 # Metrics text box location (top right corner)
 x_metrics_location = ticks[-1] - 1
 max_bin_value = 0
 for key in reads_distr[bar].keys():
-	if reads_distr[bar][key] > max_bin_value:
-		max_bin_value = reads_distr[bar][key]
+    if reads_distr[bar][key] > max_bin_value:
+        max_bin_value = reads_distr[bar][key]
 y_metrics_location = max_bin_value - 1
 
 ##### Plotting bar chart
@@ -327,29 +341,29 @@ plt.rcParams["figure.figsize"] = (width,height)
 xlabel = 'Read sizes'
 ylabel = None
 if bar == 'sum':
-	ylabel = 'Total bases (in Mb)'
+    ylabel = 'Total bases (in Mb)'
 elif bar == 'count':
-	ylabel = 'Total read count'
+    ylabel = 'Total read count'
 
 if title is None:
-	title = os.path.basename(fastq)
+    title = os.path.basename(fastq)
 
 plt.title(
-	title,
-	loc='center',
-	fontsize = 12,
-	y = 1.0,
-	pad=-50,
-	fontweight=title_font
+    title,
+    loc='center',
+    fontsize = 12,
+    y = 1.0,
+    pad=-50,
+    fontweight=title_font
 )
 plt.text(
-	x_metrics_location,
-	y_metrics_location,
-	metrics,
-	fontsize=10,
-	family='monospace',
-	va='top',
-	ha='right'
+    x_metrics_location,
+    y_metrics_location,
+    metrics,
+    fontsize=10,
+    family='monospace',
+    va='top',
+    ha='right'
 )
 
 plt.xlabel(xlabel)
@@ -357,14 +371,20 @@ plt.xlim(0,num_bins)
 plt.xticks(ticks,labels)
 plt.ylabel(ylabel)
 plt.yscale(yscale)
-plt.bar(list(reads_distr[bar].keys()), reads_distr[bar].values(), color=rgb, align='edge')
+
+plt.bar(
+    list(reads_distr[bar].keys()),
+    reads_distr[bar].values(),
+    color=rgb,
+    align='edge'
+)
 
 # Output either to matplotlib GUI or file
 if output is None:
-	plt.show()
+    plt.show()
 else:
-	for x in output:
-		filename = outdir + '/' + x 
-		print(f"Creating {filename}...")
-		plt.savefig(filename)
-	print("")
+    for x in output:
+        filename = outdir + '/' + x 
+        print(f"Creating {filename}...")
+        plt.savefig(filename)
+    print("")
